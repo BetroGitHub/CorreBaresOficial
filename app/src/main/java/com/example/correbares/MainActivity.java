57fdea6c32352325887d6703fd2f5b58fc48c1c7
fl0py <manubzs748@gmail.com>
2022-06-01 10:43:11 +0200

    holapene

--- app/src/main/java/com/example/correbares/MainActivity.java
+++ app/src/main/java/com/example/correbares/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //pueba
+        //pueba beto me la come to benne
     }
 }
